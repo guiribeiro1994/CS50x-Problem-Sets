b88790f324d6cbbd3f44589a18f58f3ed86ae05e
guiribeiro1994 <63989096+guiribeiro1994@users.noreply.github.com>
2020-05-18 17:52:51 -0500

    Add files via upload

---
+++ mario.c
@@ -0,0 +1,80 @@
+// Mario pyramid blocks problem set cs50 wk1 2020
+// compile as: clang mario.c -o mario -lcs50
+
+// Header files go here.
+#include <stdio.h>
+#include <cs50.h>
+
+// Function prototypes go here.
+int getsHeight();
+void pyramidStep(int a, int b);
+
+
+
+// Program start.
+int main()
+{
+  // Gets user input.
+  int h = getsHeight(), space = h - 1, block = h - space;
+
+  // Prints pyramid.
+  for (size_t i = 0; i < h; i++) {
+    pyramidStep(space, block);
+    space--;
+    block++;
+  }
+
+  return 0;
+}
+// Program end.
+
+
+
+// getsHeight function definition.
+int getsHeight()
+{
+  // Asks user for desired height.
+  int n = get_int("Please input a height between 1-8, inclusively: ");
+
+  // Error message if constraints not met.
+  while (n < 1 || n > 8)
+  {
+      if (n < 1 || n > 8)
+      {
+          printf("Height must be a number between 1-8, inclusive. Please try again.\n");
+          n = get_int("Height: ");
+      }
+  }
+  return n;
+}
+
+
+// pyramidStep function definition
+void pyramidStep(int a, int b)
+{
+  // Left spaces.
+  for (size_t i = 0; i < a; i++) {
+    printf("%c", 32);
+  }
+
+  // Left blocks.
+  for (size_t i = 0; i < b; i++) {
+    printf("%c", 35);
+  }
+
+  // Middle break.
+  printf("%c%c", 32, 32);
+
+  // Right blocks.
+  for (size_t i = 0; i < b; i++) {
+    printf("%c", 35);
+  }
+
+  // Right spaces.
+  for (size_t i = 0; i < a; i++) {
+    printf("%c", 32);
+  }
+
+  // Line break.
+  printf("\n");
+}
